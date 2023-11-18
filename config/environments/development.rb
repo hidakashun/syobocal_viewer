5c9a04e1904235c17e6d54369b0832d56b2fcbd3
hidakashun <hidakashun60@gmail.com>
2023-11-18 15:02:20 +0000

    [add]環境設定、プラグイン

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "a8e5aa1199b346b6b347f5fcfe5afcc5.vfs.cloud9.ap-southeast-2.amazonaws.com"
 end
